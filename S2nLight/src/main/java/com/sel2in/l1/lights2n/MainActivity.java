e4a99e2be218e4a9022652d68c5ffa6effa3564d
Tushar Kapila <tgkprog@gmail.com>
2015-09-19 14:35:22 +0530

    no UI

--- S2nLight/src/main/java/com/sel2in/l1/lights2n/MainActivity.java
+++ S2nLight/src/main/java/com/sel2in/l1/lights2n/MainActivity.java
@@ -24,7 +24,7 @@ import android.hardware.Camera.Parameters;
 import android.util.Log;
 import android.view.WindowManager;
 import android.widget.CheckBox;
-import android.widget.TextView;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,11 +38,7 @@ public class MainActivity extends AppCompatActivity {
      */
     SectionsPagerAdapter mSectionsPagerAdapter;
     Camera camera;
-    //TextView txtPos;
-    private boolean isLighOn = false;
-    private boolean isFlashOn;
-    private boolean hasFlash;
-    CheckBox flashLeave;
+    CheckBox screenNormalBacklightOff;
     Parameters params;
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -50,15 +46,20 @@ public class MainActivity extends AppCompatActivity {
     ViewPager mViewPager;
     PowerManager.WakeLock wl;
     boolean initDone = false;
-    private boolean isFlashWasOn;
     long cnt = -1;
+    AlertDialog alert;
+    int pos1 = -1;
+    //TextView txtPos;
+    private boolean isLighOn = false;
+    private boolean isFlashOn;
+    private boolean hasFlash;
+    private boolean isFlashWasOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
     }
-
 
     public void flashTogggle(View v) {
         //showDialog("oChange ", "cnt " + cnt + " f " + isFlashOn );
@@ -77,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void cameraFlashSetup(Context context) {
-        flashLeave = (CheckBox) findViewById(R.id.flashLeaveOn);
+        screenNormalBacklightOff = (CheckBox) findViewById(R.id.backLightDontLeaveOn);
         //showDialog("cameraFlashSetup ", "a cnt " + cnt );
         PackageManager pm = context.getPackageManager();
         // First check if device is supporting flashlight or not
@@ -90,8 +91,6 @@ public class MainActivity extends AppCompatActivity {
         getCamera();
 
     }
-
-    AlertDialog alert;
 
     private void showDialog(String ttle, String msg) {
         if (alert == null) {
@@ -112,14 +111,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         isFlashWasOn = false;
-        if (flashLeave == null || !flashLeave.isChecked()) {
-            if (isFlashOn) {
-                isFlashWasOn = true;
-                turnOffFlash();
-            } else {
-                isFlashWasOn = false;
-            }
+        if (isFlashOn) {
+            isFlashWasOn = true;
+            turnOffFlash();
+        } else {
+            isFlashWasOn = false;
         }
+
     }
 
     @Override
@@ -156,6 +154,20 @@ public class MainActivity extends AppCompatActivity {
         if (!initDone) {
             initDone = true;
             try {
+
+                screenNormalBacklightOff = (CheckBox) findViewById(R.id.backLightDontLeaveOn);
+                screenNormalBacklightOff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if (isChecked) {
+                            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                        } else {
+                            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                        }
+
+                    }
+                });
                 try {
                    /* txtPos = (TextView)findViewById(R.id.textView2);
                     if(txtPos != null) {
@@ -212,7 +224,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     private void turnOffFlash() {
         Log.e("s2ner", "fl off ");
         //showDialog("off", "turnOffFlash isFlash " + isFlashOn + ", has " + this.hasFlash);
@@ -237,24 +248,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    int pos1 = -1;
-
     void positionAt(int position) {
         WindowManager.LayoutParams layout = getWindow().getAttributes();
         pos1 = position;
         // if(txtPos != null) {
         //txtPos.setText(pos1 + " a");
         // }
+        if (screenNormalBacklightOff == null || (screenNormalBacklightOff != null && screenNormalBacklightOff.isChecked() == false)) {
+            if (position == 1) {
+                this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            } else {
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        }
         if (position == 0 || position == 8) {
             layout.screenBrightness = 1F;
         } else if (position == 1 || position == 7) {
             layout.screenBrightness = 0.5F;
-        } else if (position == 3 || position == 6) {
+        } else if (position == 2 || position == 6) {
             layout.screenBrightness = 0.2F;
-        } else if (position == 4) {
+        } else if (position == 3) {
             layout.screenBrightness = 0.1F;
-        } else if (position == 5) {
+        } else if (position == 4) {
             layout.screenBrightness = 0.0F;
+        } else if (position == 5) {
+            layout.screenBrightness = -1F;
         }
         getWindow().setAttributes(layout);
     }
@@ -296,78 +314,6 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public void setPrimaryItem(ViewGroup container, int position, Object object) {
-            super.setPrimaryItem(container, position, object);
-            positionAt(position);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            return 9;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
-            return "s2n " + position;
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-
-    }
-
-
     @Override
     protected void onStop() {
         super.onStop();
@@ -393,4 +339,78 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        static PlaceholderFragment MAIN1;
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            if(MAIN1 == null) MAIN1 = new PlaceholderFragment();
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public void setPrimaryItem(ViewGroup container, int position, Object object) {
+            super.setPrimaryItem(container, position, object);
+            positionAt(position);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+
+            return PlaceholderFragment.newInstance(1);
+        }
+
+        @Override
+        public int getCount() {
+            return 9;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            return "s2n " + position;
+        }
+    }
+
+
 }
